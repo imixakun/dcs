e3c18b066c470c9ac41981169558d62eec5b9eeb
LLW <119773748+imixakun@users.noreply.github.com>
2023-10-21 20:12:16 +0500

    Add files via upload

--- dcs.py
+++ dcs.py
@@ -10,7 +10,7 @@ client = TelegramClient('anon', api_id, api_hash)
 @client.on(events.NewMessage)
 async def my_event_handler(event):
     if 'attack' in event.raw_text:
-        for attack in range(1000):
+        while 1:
             await client.send_message(f"@{username}", "Error: 958\nPlease, check your security! by T958")
 
 client.start()
